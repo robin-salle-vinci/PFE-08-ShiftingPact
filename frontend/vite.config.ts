41607cb1ec99be1c07bdf318068aecdf243179a4
Willom125 <chisom.ubah@student.vinci.be>
2024-12-03 23:52:48 +0100

    docker front completed

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,6 +6,10 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    port: 4040,
+    host: true,
+  },
   plugins: [
     vue(),
     vueDevTools(),
