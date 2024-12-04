b43c9c76e8841d9e2b5713b67ce2512c76709b21
Xavier-Msrt <xaviermassart@outlook.com>
2024-12-04 11:54:38 +0100

    feat: add base db connection

--- backend/myapp/views.py
+++ backend/myapp/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
+from django.template.defaultfilters import date
+
+from myapp.models import Person
 
 
 # Create your views here.
@@ -10,4 +13,12 @@ def exemple(request):
     data = {
         'message': 'Hello world'
     }
+    person = Person(
+        email="john.doe@example.com",
+    )
+    person.save()
     return JsonResponse(data)
+
+
+
+
