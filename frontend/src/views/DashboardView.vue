7ecee635b64dc9d83cf607a0fb7569c873c80646
xavier-massart-vinci <xavier.massart@student.vinci>
2024-12-05 17:18:44 +0100

    feat: styles form

--- frontend/src/views/DashboardView.vue
+++ frontend/src/views/DashboardView.vue
@@ -1,14 +1,14 @@
 <template>
   <HeaderElement />
   <main>
-    <div class="card">
-      <div v-if="!submited">
+    <div class="card" v-if="!submited">
+      <div>
         <h2>Enregistrer une nouvelle entreprise</h2>
         <form @submit.prevent="handleCreate">
-          <input type="text" name="firstname" placeholder="Prenom de l'employee" />
+          <!--<input type="text" name="firstname" placeholder="Prenom de l'employee" />
           <input type="text" name="lastname" placeholder="Nom de l'employee" />
           <input type="text" name="email" placeholder="Email de l'employee" v-model="email" />
-          <!--<input
+          <input
             type="text"
             name="employee-function"
             id="employee-function"
@@ -26,18 +26,65 @@
             id="Chiffre d'affaire du derrnier exercises comptable"
           />
           <input type="text" placeholder="Nombre de travailleurs" />-->
+          <div class="input-element">
+            <label for="company-name">Nom de l'entreprise</label>
+            <input
+              id="company-name"
+              type="text"
+              name="company-name"
+              placeholder="Nom de la PME"
+              v-model="companyName"
+            />
+          </div>
+
+          <div class="input-element">
+            <label for="number-worker">Nombre d'employee</label>
+            <input
+              id="number-worker"
+              type="number"
+              name="number-worker"
+              placeholder="Nombre d'employee"
+              v-model="numberWorker"
+            />
+          </div>
+          <div class="input-element">
+            <label for="building-owner">La societe dispose d'une propriétaire</label>
+            <input
+              id="building-owner"
+              type="checkbox"
+              name="building-owner"
+              v-model="buildingOwner"
+            />
+          </div>
+
+          <div class="input-element">
+            <label for="product-service">La société propose des services</label>
+            <input
+              id="product-service"
+              type="checkbox"
+              name="product-service"
+              v-model="isService"
+            />
+          </div>
+
           <input type="submit" value="Générer un accès client" />
         </form>
       </div>
-      <div v-else>
-        <h2>Identifiant de connection de l'entreprise</h2>
-        <div style="display: flex; flex-direction: column; justify-content: left">
-          <label for="username">Nom d'utilisateur</label>
-          <input type="text" name="username" placeholder="nom d'utilisateur" />
+    </div>
+    <div v-else class="card">
+      <h2>Identifiant de connection de l'entreprise</h2>
+      <div class="cred-container">
+        <div class="cred-element">
+          <p>
+            Identifiant: <strong>{{ identifier }}</strong>
+          </p>
+          <button>copy</button>
         </div>
-        <div>
-          <label for="password">Mots de passe</label>
-          <input type="text" name="password" placeholder="Mots de passe" />
+        <div class="cred-element">
+          <p>
+            Mot de passe: <strong>{{ password }}</strong>
+          </p>
+          <button>copy</button>
         </div>
       </div>
     </div>
@@ -46,25 +93,38 @@
 
 <script setup lang="ts">
   import HeaderElement from '@/components/HeaderElement.vue'
-  import axios, { type AxiosResponse } from 'axios'
   import { ref } from 'vue'
 
   const apiUrl = import.meta.env.BASE_URL
   const submited = ref<boolean>(false)
-  const email = ref<string>('')
+
+  const companyName = ref<string>('')
+  const numberWorker = ref<number>(0)
+  const buildingOwner = ref<boolean>(false)
+  const isService = ref<boolean>(false)
+  const password = ref<string>('')
+  const identifier = ref<string>('')
 
   const handleCreate = () => {
+    if (!companyName.value) return
+
     submited.value = true
-    axios
-      .post(`${apiUrl}/client`, {
-        // TODO SEND DATA
-      })
-      .then((response: AxiosResponse) => {
-        submited.value = true
-        // TODO display credentials
-        console.log(response)
-      })
-      .catch(() => {})
+    console.log(apiUrl)
+    console.log('Company name:', companyName.value)
+    console.log('Number worker:', numberWorker.value)
+    console.log('Building owner:', buildingOwner.value)
+    console.log('Is service:', isService.value ? 'Societe service' : 'Societe produit')
+
+    //   axios
+    //     .post(`${apiUrl}/client`, {
+    //       // TODO SEND DATA
+    //     })
+    //     .then((response: AxiosResponse) => {
+    //       submited.value = true
+    //       // TODO display credentials
+    //       console.log(response)
+    //     })
+    //     .catch(() => {})
   }
 </script>
 
@@ -82,12 +142,75 @@
     box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
     text-align: center;
     width: 100%;
-    max-width: 500px;
+    max-width: 800px;
   }
 
   form {
     display: flex;
     flex-direction: column;
+    text-align: left; /* Permet d'aligner les labels à gauche */
+  }
+  .input-element {
+    display: flex;
+    flex-direction: row; /* Aligne les éléments en ligne */
+    align-items: center;
+    margin-top: 1rem;
+    gap: 1rem;
+    width: 100%; /* Assure que l'élément prend toute la largeur */
+  }
+
+  .input-element input[type='checkbox'] {
+    flex: 0; /* Empêche la checkbox de s'étendre */
+    width: 20px; /* Largeur fixe */
+    height: 20px;
+    margin: 0; /* Pas de marge supplémentaire */
+  }
+
+  .input-element label {
+    flex: 1; /* Les labels occupent une partie de la ligne */
+    text-align: left; /* Texte aligné à gauche */
+  }
+
+  .input-element input[type='text'],
+  .input-element input[type='number'] {
+    flex: 2; /* Les champs texte/numériques prennent plus d'espace */
+  }
+
+  input[type='checkbox'] {
+    appearance: none;
+    -webkit-appearance: none;
+    width: 20px;
+    height: 20px;
+    border: 2px solid #013238;
+    border-radius: 4px;
+    transition:
+      background-color 0.3s ease,
+      border-color 0.3s ease;
+    cursor: pointer;
+    position: relative;
+  }
+
+  input[type='checkbox']::after {
+    content: '';
+    display: block;
+    width: 10px;
+    height: 10px;
+    background-color: transparent;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    border-radius: 2px;
+    transition: background-color 0.3s ease;
+  }
+
+  input[type='checkbox']:checked {
+    border-color: #013238;
+    background-color: #013238;
+  }
+
+  input[type='checkbox']:checked::after {
+    background-color: white;
   }
 
   input {
@@ -111,5 +234,21 @@
     border-radius: 4px;
     cursor: pointer;
     transition: all 0.3s ease;
+  }
+
+  /* Credentials */
+
+  .cred-element {
+    display: flex;
+    justify-content: space-around;
+    padding: 2%;
+  }
+  .cred-element button {
+    background-color: #013238;
+    color: white;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+    width: 70px;
   }
 </style>
