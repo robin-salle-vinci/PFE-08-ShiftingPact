24787f5bfa73bd19f84a5cf6321d25c91edbd9b6
xavier-massart-vinci <xavier.massart@student.vinci>
2024-12-08 13:01:37 +0100

    feat: create an esg

--- backend/esg/urls.py
+++ backend/esg/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    #path('', views.create_esg, name='create_esg_view')
+    path('', views.create_esg_views, name='create_esg_views')
 ]
--- backend/esg/views.py
+++ backend/esg/views.py
@@ -1,5 +1,31 @@
+from django.http import JsonResponse
 from django.shortcuts import render
+from django.views.decorators.http import require_POST
+from users.models import  Users
+from esg.models import ModulesESG
+from datetime import datetime
+import json
 
-# Create your views here.
+@require_POST
+def create_esg_views(request):
+    try:
+      data = json.loads(request.body)
+      id_client = data.get('idClient')
 
-
+      # check if client exist
+      if not id_client:
+          return JsonResponse({'message': 'Client id is required'}, status=400)
+      if Users.objects.filter(id=id_client).count() == 0:
+          return JsonResponse({'message': 'Client does not exist'}, status=400)
+          
+      ModulesESG.objects.create(
+          id_client=id_client,
+          date_last_modification=datetime.today().date(),
+          original_answers=[],
+          modified_answers=[],
+          state="open",
+      )
+      return JsonResponse({'message': 'Module ESG created successfully'}, status=201)
+    
+    except Exception as e:
+        return JsonResponse({'message': str(e)}, status=500)
