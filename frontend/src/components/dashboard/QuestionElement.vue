58f132d72255522608607a1d562eeaefb7bbb6d0
xavier-massart-vinci <xavier.massart@student.vinci>
2024-12-08 12:16:13 +0100

    feat: form employee

--- frontend/src/components/dashboard/QuestionElement.vue
+++ frontend/src/components/dashboard/QuestionElement.vue
@@ -1,36 +1,82 @@
 <script setup lang="ts">
   import { defineProps, ref } from 'vue'
 
-  interface Question {
-    question: string
-    reponse: string
-    commentaire: string
-    isEngagement: boolean
+  interface Choice {
+    id: string
+    indexation_choice: string
     value: string
-    choix: string[]
+    score: number
+  }
+
+  interface Question {
+    id: string
+    indexation_question: string
+    template: string
+    value: string
+    type_response: string
+    choices: Choice[]
   }
 
   const props = defineProps<{ question: Question }>()
+  console.log(props.question)
 
   const questionModel = ref(props.question)
 
-  const handleSave = () => {
-    console.log('save')
-  }
+  // const handleSave = () => {
+  //   console.log('save')
+  // }
 </script>
 
 <template>
-  <div class="card">
-    <span>{{ question.question }}</span>
-    <form class="inputs">
-      <input v-model="questionModel.reponse" type="text" />
-      <textarea v-model="questionModel.commentaire" readonly disabled></textarea>
-      <span>{{ questionModel.isEngagement }}</span>
-      <span>{{ questionModel.value }}</span>
-      <span>{{ questionModel.choix }}</span>
-      <button @click="handleSave">sauvgarder</button>
-    </form>
+  <div>
+    <!-- {{ questionModel }} -->
+
+    <div>{{ questionModel.value }}</div>
+    <div>
+      <div>reponse du client TODO</div>
+      <div v-if="questionModel.choices.length > 0">
+        <select v-model="questionModel.value">
+          <option v-for="choix in questionModel.choices" :key="choix.id" :value="choix">
+            {{ choix.value }}
+          </option>
+        </select>
+      </div>
+      <div v-else>
+        <input v-model="questionModel.value" type="text" />
+      </div>
+
+      <div>todo commentaire</div>
+    </div>
   </div>
+  <!-- <div class="card">
+
+     <span>{{ questionModel.question }}</span>
+    <form class="inputs">
+      <div v-if="!questionModel.choix.length">
+        <input v-model="questionModel.reponse" type="text" />
+      </div>
+      <textarea v-model="questionModel.commentaire" readonly disabled></textarea>
+      <div>
+        <label>
+          <input type="radio" v-model="questionModel.isEngagement" :value="false" />
+          Déjà en application
+        </label>
+        <label>
+          <input type="radio" v-model="questionModel.isEngagement" :value="true" />
+          Engagement
+        </label>
+      </div>
+      <span>{{ questionModel.value }}</span>
+      <div v-if="questionModel.choix.length">
+        <select v-model="questionModel.value">
+          <option v-for="choix in questionModel.choix" :key="choix" :value="choix">
+            {{ choix }}
+          </option>
+        </select>
+      </div>
+      <button @click="handleSave">sauvgarder</button>
+    </form> 
+  </div> -->
 </template>
 
 <style>
@@ -49,8 +95,6 @@
     margin-top: 2%;
     display: flex;
     justify-content: space-around;
-    /* same linge flex elem*/
-
     gap: 10px;
   }
   textarea {
--- frontend/src/views/EsgView.vue
+++ frontend/src/views/EsgView.vue
@@ -5,8 +5,20 @@
   <div class="container">
     <h1>Questionaires de l'entreprise NOM_ENTREPRISE {{ id }}</h1>
 
-    <div class="questions-container" v-for="(question, index) in questions" :key="index">
-      <QuestionElement :question="question" />
+    <div v-for="challenge in questionnaire[0].challenges" :key="challenge.id">
+      <h2>{{ challenge.value }}</h2>
+
+      <div v-for="subChallenge in challenge.sub_challenges" :key="subChallenge.id">
+        <h3>{{ subChallenge.value }}</h3>
+
+        <div
+          class="questions-container"
+          v-for="question in subChallenge.questions"
+          :key="question.id"
+        >
+          <QuestionElement :question="question" />
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -15,20 +27,100 @@
   import QuestionElement from '@/components/dashboard/QuestionElement.vue'
   import HeaderElement from '@/components/structure/HeaderElement.vue'
   import router from '@/router'
+  // import axios from 'axios'
   import { ref } from 'vue'
   import { useRoute } from 'vue-router'
 
   const route = useRoute()
   const id = route.params.id
+  // const apiUrl = import.meta.env.VITE_API_URL
+  // axios call to get questions
+  // const questionDb = await axios.get(`${apiUrl}/questions`)
+  // console.log(questionDb)
 
-  const questions = ref([
+  const questionnaire = ref([
     {
-      question: 'Combien de cheval on peux mettre dans un appartement ?',
-      reponse: 'Reponse 1',
-      commentaire: 'Commentaire 1',
-      isEngagement: true,
-      value: '400',
-      choix: ['oui', 'non'],
+      challenges: [
+        {
+          id: '1',
+          indexation_challenge: 'CH001',
+          value: 'Challenge 1',
+          color: 'blue',
+          sub_challenges: [
+            {
+              id: '101',
+              indexation_sub_challenge: 'SC001',
+              value: 'Sub-challenge 1',
+              questions: [
+                {
+                  id: '1001',
+                  indexation_question: 'Q001',
+                  template: 'Template for question 1',
+                  value: 'Question 1 text',
+                  type_response: 'text',
+                  choices: [
+                    {
+                      id: '2001',
+                      indexation_choice: 'C001',
+                      value: 'Choice 1 for question 1',
+                      score: 10,
+                    },
+                    {
+                      id: '2002',
+                      indexation_choice: 'C002',
+                      value: 'Choice 2 for question 1',
+                      score: 5,
+                    },
+                  ],
+                },
+                {
+                  id: '1002',
+                  indexation_question: 'Q002',
+                  template: 'Template for question 2',
+                  value: 'Question 2 text',
+                  type_response: 'multiple_choice',
+                  choices: [
+                    {
+                      id: '2003',
+                      indexation_choice: 'C003',
+                      value: 'Choice 1 for question 2',
+                      score: 8,
+                    },
+                    {
+                      id: '2004',
+                      indexation_choice: 'C004',
+                      value: 'Choice 2 for question 2',
+                      score: 3,
+                    },
+                  ],
+                },
+              ],
+            },
+            {
+              id: '102',
+              indexation_sub_challenge: 'SC002',
+              value: 'Sub-challenge 2',
+              questions: [
+                {
+                  id: '1003',
+                  indexation_question: 'Q003',
+                  template: 'Template for question 3',
+                  value: 'Question 3 text',
+                  type_response: 'text',
+                  choices: [],
+                },
+              ],
+            },
+          ],
+        },
+        {
+          id: '2',
+          indexation_challenge: 'CH002',
+          value: 'Challenge 2',
+          color: 'green',
+          sub_challenges: [],
+        },
+      ],
     },
   ])
 
