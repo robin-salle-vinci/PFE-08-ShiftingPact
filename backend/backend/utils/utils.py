3c3d65482b282c3ed57511461406c3fe43e66ba8
unknown <chisom.ubah@student.vinci.be>
2024-12-08 15:20:06 +0100

    to be continued

--- backend/backend/utils/utils.py
+++ backend/backend/utils/utils.py
@@ -2,8 +2,10 @@ import jwt
 import environ
 from datetime import datetime, timedelta
 
+from django.http import JsonResponse
+
 from questions.models import Challenges, SubChallenges, Questions, Choices, Answers
-from users.models import ClientInformation
+from users.models import ClientInformation, Users
 
 env = environ.Env()
 environ.Env.read_env()
@@ -51,6 +53,25 @@ def decode_token(token):
   except jwt.InvalidTokenError:
     raise jwt.InvalidTokenError("Invalid token")
 
+def check_authenticated_user(request):
+    header = request.headers.get('Authorization')
+    if not header or not header.startswith('Bearer '):
+      return JsonResponse({'error': 'Invalid Authorization header'}, status=400)
+
+    token = header.split(' ')[1]
+    if token is None:
+      return JsonResponse({'error': 'Token is missing'}, status=401)
+
+    try:
+      user_payload = decode_token(token)
+    except Exception as e:
+      return JsonResponse({'error': str(e)}, status=401)
+
+    user = Users.get_by_id(user_payload['id'])
+
+    if user is None:
+      return JsonResponse({'Not Found User'}, status=404)
+    return user
 
 def client_info_json(client_info):
   return \
@@ -133,14 +154,27 @@ def module_json(module):
       'original_answers':
         [
           answer_json(answer)
-          for answer in (Answers.get_by_id(idAnswer) for idAnswer in module.original_answers)
+          for answer in (Answers.get_by_id(id_answer) for id_answer in module.original_answers)
         ],
       'modified_answers':
         [
           answer_json(answer)
-          for answer in (Answers.get_by_id(idAnswer) for idAnswer in module.modified_answers)
+          for answer in (Answers.get_by_id(id_answer) for id_answer in module.modified_answers)
         ],
       'state': module.state,
       'calculated_score': module.calculated_score
     }
 
+def commitment_json(commitment):
+  return \
+    {
+    'id': str(commitment.id),
+    'client_information': client_info_json(ClientInformation.get_by_id(commitment.id_client)),
+    'created_date': commitment.created_date.isoformat(),
+    'answers_commitments':
+      [
+        answer_json(answer)
+        for answer in (Answers.get_by_id(id_answer) for id_answer in commitment.answers_commitments)
+      ],
+    'calculated_score': float(commitment.calculated_score),
+  }
--- backend/commitments/views.py
+++ backend/commitments/views.py
@@ -1,9 +1,9 @@
 from django.http import JsonResponse
 from django.views.decorators.http import require_GET
 
+from backend.utils.utils import commitment_json, check_authenticated_user
 from commitments.models import CommitmentPacts
 from users.models import Users
-from backend.utils.token_utils import decode_token
 
 
 # Create your views here.
@@ -12,23 +12,10 @@ from backend.utils.token_utils import decode_token
 @require_GET
 def get_one(request, id):
    try:
-      header = request.headers.get('Authorization')
-      if not header or not header.startswith('Bearer '):
-         return JsonResponse({'error': 'Invalid Authorization header'}, status=400)
 
-      token = header.split(' ')[1]
-      if token is None:
-         return JsonResponse({'error': 'Token is missing'}, status=401)
-
-      try:
-         user_payload = decode_token(token)
-      except Exception as e:
-         return JsonResponse({'error': str(e)}, status=401)
-
-      user = Users.get_by_id(user_payload['id'])
-
-      if user is None:
-         return JsonResponse({'Not Found User'}, status=404)
+      user = check_authenticated_user(request)
+      if isinstance(user, JsonResponse):
+         return user
 
       commitment = CommitmentPacts.get_by_id(id)
 
@@ -38,7 +25,8 @@ def get_one(request, id):
       if not (commitment.id_client == user.id_client_information or user.role == 'employee'):
          return JsonResponse({'error': 'Not Authorized'}, status=403)
 
-      commitment_data = {}
+      commitment_data = commitment_json(commitment)
+
       return JsonResponse(commitment_data, safe=False, status=200)
 
    except Exception as e:
