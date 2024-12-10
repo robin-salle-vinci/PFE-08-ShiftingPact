b76ac3e09bf660fb79eb5ad6bfabb6971e6eeee6
Sallé Robin <robin.salle@student.vinci.be>
2024-12-10 15:08:27 +0100

    refactor name road modules app

--- backend/modules/urls.py
+++ backend/modules/urls.py
@@ -7,10 +7,10 @@ urlpatterns = [
     path('',views.read_modules, name='read_modules'),
 
     # Get one ESG module for 
-    path('esg/<str:uuid_module_esg>', views.read_module_by_esg_id, name='read_module_by_esg_id'),
-    path('esgs/<str:uuid_client>', views.read_modules_by_client_id, name='read_modules_by_client_id'),
+    path('module/<str:uuid_module_esg>', views.read_module_by_esg_id, name='read_module_by_esg_id'),
+    path('client/<str:uuid_client>', views.read_modules_by_client_id, name='read_modules_by_client_id'),
     # Get one ESG module by client id most recent
-    path('client/<str:uuid_client>', views.read_module_by_client_id, name='read_module_by_client_id'),
+    path('module/client/<str:uuid_client>', views.read_module_by_client_id, name='read_module_by_client_id'),
     # Create ESG module
     path('create/<str:uuid_client>', views.create_esg_views, name='create_esg_views'),
 
